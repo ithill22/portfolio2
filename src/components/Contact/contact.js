64ff8cb4ad93c221c657f0b8fb68b1972fd08477
Isaac Thill <ithill22@turing.edu>
2024-06-13 11:10:20 -0600

    alt color for website

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./contact.css";
 import resumeImage from "../../assets/resume.png";
 import LinkedInIcon from "../../assets/linkedin.png";
@@ -7,6 +7,11 @@ import emailjs from '@emailjs/browser';
 
 const Contact = () => {
   const form = useRef();
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
   const resumePdfPath = process.env.PUBLIC_URL + '/resume_master.pdf';
 
   const sendEmail = (e) => {
@@ -20,28 +25,58 @@ const Contact = () => {
         () => {
           console.log('SUCCESS!');
           form.current.reset();
+          setFormData({ name: '', email: '', message: '' }); // Reset form data
         },
         (error) => {
           console.log('FAILED...', error.text);
         },
       );
   };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const isFormValid = formData.name && formData.email && formData.message;
+
   return (
     <section id="contactPage">
       <div id="resume">
         <h1 className="contactPageTitle">My Resume</h1>
-        <a href={ resumePdfPath } target="_blank" rel="noreferrer">
-          <img src={ resumeImage } alt="Resume" className="resumeImg" />
+        <a href={resumePdfPath} target="_blank" rel="noreferrer">
+          <img src={resumeImage} alt="Resume" className="resumeImg" />
         </a>
       </div>
       <div id="contact">
         <h1 className="contactPageTitle">Contact Me</h1>
         <span className="contactDesc">Please fill out the form below to discuss any work opportunities</span>
         <form className="contactForm" ref={form} onSubmit={sendEmail}>
-          <input type="text" className="name" placeholder="Your Name" name='from_name' />
-          <input type="email" className="email" placeholder="Your Email" name='from_email'/>
-          <textarea className="msg" name="message" rows="5" placeholder="Your Message"></textarea>
-          <button type="submit" value="Send" className="submitBtn">Submit</button>
+          <input
+            type="text"
+            className="name"
+            placeholder="Your Name"
+            name='from_name'
+            value={formData.name}
+            onChange={handleChange}
+          />
+          <input
+            type="email"
+            className="email"
+            placeholder="Your Email"
+            name='from_email'
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <textarea
+            className="msg"
+            name="message"
+            rows="5"
+            placeholder="Your Message"
+            value={formData.message}
+            onChange={handleChange}
+          />
+          <button type="submit" className="submitBtn" disabled={!isFormValid}>Submit</button>
           <div className="links">
             <a href="https://www.linkedin.com/in/isaac-thill" target="_blank" rel="noreferrer">
               <img src={LinkedInIcon} alt="LinkedIn" className="link" />
@@ -53,7 +88,7 @@ const Contact = () => {
         </form>
       </div>
     </section>
-  )
+  );
 }
 
 export default Contact;
--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -2,25 +2,28 @@ import React from 'react';
 import './intro.css';
 import bg from '../../assets/image.png';
 import btnImg from '../../assets/hireme.png';
-import {Link} from 'react-scroll';
+import { Link } from 'react-scroll';
 
 const Intro = () => {
   return (
     <section id="intro">
-        <div className="introContent">
-            <span className="hello">Hello</span>
-            <span className="introText">I'm <span className="introName">Isaac Thill</span> <br />Software Engineer</span>
-            <p className="introPara">If you're on the hunt for IT, you've landed in the right place. I am a passionate software developer weaving intricate code and crafting captivating digital experiences. Explore my portfolio, where IT meets innovation, creativity, and precision.</p>
-            <a href="https://www.linkedin.com/in/isaac-thill" target="_blank" rel="noopener noreferrer">
-              <button className="btn">
-                <img src={btnImg} alt="Hire Me" className='btnImg' />
-                Hire Me
-              </button>
-            </a>
-        </div>
-        <img src={bg} alt="Profile" className="bg" />
+      <div className="introOverlay"></div>
+      <div className="introContent">
+        <span className="hello">Hello</span>
+        <span className="introText">I'm <span className="introName">Isaac Thill</span> <br />Software Engineer</span>
+        <p className="introPara">If you're on the hunt for IT, you've landed in the right place. I am a passionate software developer weaving intricate code and crafting captivating digital experiences. Explore my portfolio, where IT meets innovation, creativity, and precision.</p>
+        <a href="https://www.linkedin.com/in/isaac-thill" target="_blank" rel="noopener noreferrer">
+          <button className="btn">
+            <img src={btnImg} alt="Hire Me" className='btnImg' />
+            Hire Me
+          </button>
+        </a>
+      </div>
+      <img src={bg} alt="Profile" className="bg" />
     </section>
   )
 }
 
 export default Intro;
+
+
